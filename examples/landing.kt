fdd7fa4cffd33a01748a7d8210e180210b48bf1c
eliaskatt <63003033+eliaskatt@users.noreply.github.com>
2026-04-11 04:36:16 +0200

    Add landing page example

---
+++ examples/landing.kt
@@ -0,0 +1,10 @@
+screen Landing {
+  column {
+    text "Build with Kattour"
+    text "A language for clear UI construction"
+    row {
+      button "Get Started"
+      button "View Examples"
+    }
+  }
+}
