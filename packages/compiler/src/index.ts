5236b4a17bc5c3d3b6bc80db09ac5725407e09dc
eliaskatt <63003033+eliaskatt@users.noreply.github.com>
2026-05-15 04:30:18 +0200

    Export analyzer and HTML helpers from compiler

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -2,4 +2,6 @@ export * from './tokenizer';
 export * from './ast';
 export * from './expression';
 export * from './parser';
+export * from './analyzer';
+export * from './html';
 export * from './compiler';
