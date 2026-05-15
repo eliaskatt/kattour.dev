b3759d23facbe6d8a92459f1f511adb88094a703
eliaskatt <63003033+eliaskatt@users.noreply.github.com>
2026-05-15 05:31:03 +0200

    Use explicit compiler API exports

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -1,8 +1,55 @@
-export * from './tokenizer';
-export * from './ast';
-export * from './expression';
-export * from './parser';
-export * from './analyzer';
-export * from './html';
-export * from './builtins';
-export * from './compiler';
+export { tokenize, tokenizeDetailed } from './tokenizer';
+export type { Token, TokenType, TokenizeResult } from './tokenizer';
+
+export type {
+  BindingNode,
+  CompileResult,
+  ComponentCallNode,
+  ComponentNode,
+  ComputedNode,
+  Diagnostic,
+  DiagnosticSeverity,
+  EffectActionNode,
+  EffectNode,
+  ElementNode,
+  EventNode,
+  ForNode,
+  IfNode,
+  ImportNode,
+  KattourArray,
+  KattourObject,
+  KattourPrimitive,
+  KattourValue,
+  MetaNode,
+  ModuleNode,
+  PageNode,
+  ParamNode,
+  ProgramNode,
+  PropertyNode,
+  ResourceNode,
+  RouteNode,
+  SlotNode,
+  SourceLocation,
+  SourceRange,
+  StatementNode,
+  ThemeNode,
+  UINode,
+  ViewNode
+} from './ast';
+
+export { evaluateExpression, interpolateTemplate } from './expression';
+export type { RuntimeScope } from './expression';
+
+export { parse, parseDetailed } from './parser';
+export type { ParseResult } from './parser';
+
+export { analyze } from './analyzer';
+export type { AnalysisResult } from './analyzer';
+
+export { attrsToString, escapeAttribute, escapeHtml, isSafeUrl, safeUrl, tag } from './html';
+
+export { BUILTIN_MODULES, isBuiltinModule, renderBuiltinModule } from './builtins';
+export type { BuiltinRenderContext, BuiltinRenderer } from './builtins';
+
+export { compile, compileDetailed } from './compiler';
+export type { CompileOptions } from './compiler';
